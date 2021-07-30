0e2db560f10a19c970b0a71e14f51459da4b53a9
MartinaM29 <80761100+MartinaM29@users.noreply.github.com>
2021-07-30 09:53:24 +0200

    Update R_code_remote_sensing_temp.r

--- R_code_remote_sensing_temp.r
+++ R_code_remote_sensing_temp.r
@@ -1,8 +1,8 @@
-## R code
+### R code ###
 
 
-#install.packages("raster")
-library(raster) #COSA FA RASTER
+#install.packages("raster") #se ancora non si è installato il pacchetto
+library(raster) #COSA FA RASTER, il pacchetto sp, gestisce tutti i dati all'interno del softwer, in questo caso raster
 
 #setwd("~/lab/")#Linux
 setwd("C:/lab_telerilevamento/")  # Windows
@@ -10,7 +10,8 @@ setwd("C:/lab_telerilevamento/")  # Windows
 
 
 #zona di studio Riserva di Parakana
-p224r63_2011 <- brick("p224r63_2011_masked.grd") #con brick si carica l'intero blocco delle bande
+p224r63_2011 <- brick("p224r63_2011_masked.grd") #con brick si carica l'intero blocco delle bande, le "" si usano per chiamare file fuori da R
+# quindi il file "p224r63_2011" contiene tutte le bande Landsat
 #queste operazioni devono essere eseguite ogni volta che si apre R
 plot(p224r63_2011) #plot con tutte le bande, 7 grafici
 p224r63_2011 #così si vedono tutte le caratteristiche del file
@@ -28,32 +29,96 @@ p224r63_2011 #così si vedono tutte le caratteristiche del file
 ###
 
 
-##Bande Landsat
-# B1: blue 
-# B2: green
-# B3: red
+## Bande Landsat
+# B1: blu 
+# B2: verde
+# B3: rosso
 # B4: NIR (NB:infrarosso vicino, vicino perchè è vicino alla parte visibile dell'occhio umano)
 # B5: infrarosso medio, SWIR (short wave infrared)
-# B6: infrarosso termico
-# B7: infrarosso medio
+# B6: infrarosso termico (infrarosso lontano)
+# B7: infrarosso medio (altro sensore per l'infrarosso medio)
 
 
-##colorRampPalette, creazione colori sulla base della riflettanza
-cls <- colorRampPalette(c("red","pink","orange","purple")) (200) #cambia la paletta di colori
+## colorRampPalette, creazione colori sulla base della riflettanza
+cls <- colorRampPalette(c("red","pink","orange","purple")) (200) #cambia la scala di colori, il rosso da i valori più bassi di riflettanza sul rosso e i valori più alti di riflettanza sul viole, per ogni banda
+# la c() indica il vettore
 plot(p224r63_2011, col=cls)
 dev.off() #chiude i grafici, equivale alla 'x'
 
-#facciamo ora il plot della banda del blu
+# altre opzioni di paletta colori
+#cls <- colorRampPalette(c('black','grey','light grey'))(100) # cambio paletta
+#plot(p224r63_2011, col=cls)
+#dev.off()
+
+#cls <- colorRampPalette(c('dark green','light green','yellow'))(100) # cambio paletta
+#plot(p224r63_2011, col=cls)
+#dev.off()
+
+
+## plot della sola banda del blu con la color Ramp Palette predefinita
 #p224r63_2011 immagine contenente tutte le bande
-#B1_sre banda contenente la banda blu utilizzando $ come legante di due blocchi in questo caso l'immagine alla banda_1
-plot(p224r63_2011$B1_sre)#plot solo della banda del blu
+#B1_sre banda contenente la banda blu utilizzando $ come legante di due blocchi in questo caso l'immagine alla banda 1
+plot(p224r63_2011$B1_sre) 
+dev.off()
 
 #plot(p224r63_2011$B2_sre)#plot banda verde
-dev.off()
+#dev.off()
+#la stessa cosa può essere fatta con tutte le bande
 
-cl <- colorRampPalette(c('black','grey','light grey'))(100) # cambio paletta
-plot(p224r63_2011, col=cl)
-dev.off()
+## plot della banda uno con la scelta di una color ramp palette
+cl <- colorRampPalette(c('dark green','light green','yellow'))(100)
+plot(p224r63_2011$B1_sre, col=cl)
 
-cl <- colorRampPalette(c('dark green','light green','yellow'))(100) # cambio paletta
-plot(p224r63_2011, col=cl)
+## funzione par: multiframe (mf)
+#par fa un settaggio dei parametri grafici
+#vogliamo impostare un grafico con sole 2 bande una accanto all'altra
+par(mfrow=c(1,2)) #1 riga e 2 colonne
+#vediamo che il sistema è a più blocchi, pertanto viene chiamato vettore
+plot(p224r63_2011$B1_sre) 
+plot(p224r63_2011$B2_sre) 
+
+#2 righe e 1 colonna
+par(mfrow=c(2,1))
+plot(p224r63_2011$B1_sre) 
+plot(p224r63_2011$B2_sre)
+
+
+## plot delle prime 4 bande di Landsat
+par(mfrow=c(4,1)) #4 righe, 1 colonna
+plot(p224r63_2011$B1_sre) 
+plot(p224r63_2011$B2_sre)
+plot(p224r63_2011$B3_sre) 
+plot(p224r63_2011$B4_sre)
+
+#2 righe, 2 colonne
+par(mfrow=c(2,2))
+plot(p224r63_2011$B1_sre) 
+plot(p224r63_2011$B2_sre)
+plot(p224r63_2011$B3_sre) 
+plot(p224r63_2011$B4_sre)
+
+
+## per ogni banda inserire la relativa color ramp palette
+par(mfrow=c(2,2))
+clb<-colorRampPalette(c("dark blue","blue","light blue"))(100)
+plot(p224r63_2011$B1_sre, col=clb)
+clg<-colorRampPalette(c('dark green','light green','yellow'))(100)
+plot(p224r63_2011$B2_sre, col=clg)
+clr<-colorRampPalette(c('dark red','red','pink'))(100)
+plot(p224r63_2011$B3_sre, col=clr)
+cln<-colorRampPalette(c('red','orange','yellow'))(100)
+plot(p224r63_2011$B4_sre, col=cln)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
