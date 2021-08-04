2f94366a66c4e0a846669a37ba5fd5ab63a281b4
MartinaM29 <80761100+MartinaM29@users.noreply.github.com>
2021-08-04 22:51:05 +0200

    Create R_code_knitr_temp.r

---
+++ R_code_knitr_temp.r
@@ -0,0 +1,20 @@
+### R_code_knitr_temp ###
+
+# per errore su windows per tinitext
+# tinytex::install_tinytex()
+# tinytex::tlmgr_update()
+
+library(knitr) # il pacchetto può utilizzare un codice esterno per creare un report in R
+# funzione stitch 
+
+# setwd("~/lab_telerilevamento/") # Linux
+setwd("C:/lab_telerilevamento/") # Windows
+# setwd("/Users/name/Desktop/lab/") # Mac
+
+
+
+
+
+
+
+
